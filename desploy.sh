3fd789e0dfd54c1e255bb8fa33ceaede704033f6
majie <373002712@qq.com>
2024-08-23 17:06:22 +0800

    desploy.sh

---
+++ desploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+npm run build
+
+git init
+git remote add origin git@github.com:mj921/my-tools.git
+git add .
+git commit -m "desploy"
+git push origin gh-pages
+
+rm -rf dist
+rm -rf types
