395271480214fb0bb843a5d0ca8d286193d6dfbb
majie <373002712@qq.com>
2024-09-26 19:43:51 +0800

    feat: icon

---
+++ src/components/MjIcon/LoadingIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    :class="['mj-icon', 'mj-loading-icon', spin ? 'mj-icon-spin' : '']"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 1024 1024"
+    fill="currentColor"
+    :style="{
+      '--mj-icon-color': color,
+    }"
+  >
+    <path
+      d="M512 170.666667C323.477333 170.666667 170.666667 323.477333 170.666667 512s152.810667 341.333333 341.333333 341.333333 341.333333-152.810667 341.333333-341.333333h85.333334c0 235.648-191.018667 426.666667-426.666667 426.666667S85.333333 747.648 85.333333 512 276.352 85.333333 512 85.333333v85.333334z"
+    ></path>
+  </svg>
+</template>
+<script setup lang="ts">
+import type { MjIconProps } from './interface';
+defineProps<MjIconProps>();
+</script>
+<style src="./mjicon.scss" lang="scss"></style>
---
+++ src/components/MjIcon/interface.ts
@@ -0,0 +1,4 @@
+export interface MjIconProps {
+  color?: string;
+  spin?: boolean;
+}
