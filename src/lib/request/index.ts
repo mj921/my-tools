3b0cd9371b12a21e1fd48910906e83419bcb8071
majie <373002712@qq.com>
2024-10-29 10:27:14 +0800

    feat: 请求

---
+++ src/lib/request/index.ts
@@ -0,0 +1,53 @@
+export interface RequestOptions {
+  url: string;
+  method?: 'get' | 'post' | 'delete' | 'option' | 'put';
+  headers?: Record<string, string>;
+  data: Record<string, any>;
+  success?: (res?: any) => void;
+  fail?: (err?: any) => void;
+}
+
+const request = (options: RequestOptions) => {
+  return new Promise((resolve, reject) => {
+    const url = options.url;
+    const method = options.method || 'get';
+    const data = options.data;
+    const headers = options.headers;
+    const success = options.success || function () {};
+    const fail = options.fail || function () {};
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    if (headers) {
+      for (const header in headers) {
+        xhr.setRequestHeader(header, headers[header]);
+      }
+    }
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        let res = xhr.responseText;
+        try {
+          res = JSON.parse(xhr.responseText);
+        } catch (e) {
+          /* empty */
+        }
+        if (xhr.status >= 200 && xhr.status < 300) {
+          success(res);
+          resolve(res);
+        } else {
+          const error = new Error('Request failed. Returned status of ' + xhr.status);
+          (error as any).response = res;
+          fail(error);
+          reject(error);
+        }
+      }
+    };
+    if (data) {
+      xhr.send(JSON.stringify(data));
+    } else {
+      xhr.send();
+    }
+  });
+};
+
+export default request;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,8 @@ import MarkdownIcon from '@/components/MjIcon/MarkdownIcon.vue';
 import MineClearanceIcon from '@/components/MjIcon/MineClearanceIcon.vue';
 import Merge2048Icon from '@/components/MjIcon/Merge2048Icon.vue';
 import { useRouter } from 'vue-router';
+import request from '@/lib/request';
+import Day from '@/lib/day/day';
 
 const router = useRouter();
 const navs = [
@@ -53,6 +55,29 @@ const navs = [
 const jumpLink = (item: { link: string; name: string }) => {
   router.push(item.link);
 };
+if (
+  new Day().format('YYYY-MM-DD') !==
+  new Day(+(localStorage.getItem('undressbaby') || 0)).format('YYYY-MM-DD')
+) {
+  const arr = ['oUy2kNTnwOPuwOJBaSYf', 'U6XYQrpNqjmzyAl6t5cP', 'VzDzMTGMXIuTwifB8wSv'];
+  localStorage.setItem('undressbaby', Date.now() + '');
+  arr.forEach((token) => {
+    request({
+      method: 'post',
+      url: 'https://undressbaby.com/api/token/sign',
+      headers: {
+        priority: 'u=1, i',
+        'x-requested-with': 'XMLHttpRequest',
+        'content-type': 'application/json',
+      },
+      data: {
+        token,
+      },
+    })
+      .then(console.log)
+      .catch(console.dir);
+  });
+}
 </script>
 
 <template>
