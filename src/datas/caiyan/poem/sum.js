a72d6667c382c3f85f3ee8b40eb28a1eb2987120
majie <373002712@qq.com>
2025-10-15 16:52:36 +0800

    诗词

---
+++ src/datas/caiyan/poem/sum.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+const list = fs.readdirSync('.');
+// const json = {
+//   all: list.filter((el) => /^\d{8}\.json$/.test(el)).map((el) => el.replace(/\.json$/, '')),
+// };
+// let i = 0;
+// while (i < list.length) {
+//   if (/^\d{8}\.json$/.test(list[i])) {
+//     const content = fs.readFileSync(`./${list[i]}`);
+//     if (content) {
+//       json[list[i].replace(/\.json$/, '')] = JSON.parse(content);
+//     }
+//   }
+//   i++;
+// }
+fs.writeFileSync(
+  './sum.json',
+  JSON.stringify(
+    list.filter((el) => /^\d{8}\.json$/.test(el)).map((el) => el.replace(/\.json$/, '')),
+  ),
+);
